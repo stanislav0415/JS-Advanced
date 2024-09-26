1654fb0d463c38a34b85537580be1e4c14be7fa0
starboy1017 <133533226+starboy1017@users.noreply.github.com>
2024-09-26 15:03:56 +0300

    a little bit more added work

---
+++ 05.JS-Advanced-DOM-Events-Exercise-Resources/02. Time-Converter/solution.js
@@ -0,0 +1,39 @@
+function attachEventsListeners() {
+
+    const inputElDays = document.querySelector('#days')
+    const inputElHours = document.querySelector('#hours')
+    const inputElMinutes = document.querySelector('#minutes')
+    const inputElCeconds = document.querySelector('#seconds')
+
+    const buttons = document.querySelectorAll('input[type="button"]')
+
+    const units = {
+        days: 86400,
+        hours: 3600,
+        minutes: 60,
+        seconds: 1
+    }
+
+    function updateUnits(secondsAmount) {
+        inputElDays.value = Number(secondsAmount / units.days).toFixed(2)
+        inputElHours.value = Number(secondsAmount / units.hours).toFixed(2)
+        inputElMinutes.value = Number(secondsAmount / units.minutes).toFixed(2)
+        inputElCeconds.value = Number(secondsAmount / units.seconds).toFixed(2)
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+
+            const inputTimeType = e.target.getAttribute('id').split('Btn')[0]
+            const currentInputEl = document.querySelector('#' + inputTimeType)
+            const currentValue = currentInputEl.value;
+
+            const multiplier = units[inputTimeType]
+
+            updateUnits(currentValue * multiplier)
+
+        })
+    })
+
+
+}
